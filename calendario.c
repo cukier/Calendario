9f99b8f8fe7be9a572e26ae0e8bf24f9532e8430
cuki <mauricio@krebs.com.br>
2013-07-25 16:32:47 -0300

    adicionado inicializacao do calendario

--- calendario.c
+++ calendario.c
@@ -50,6 +50,32 @@ int ler_dado(int addr) {
 	return leitura;
 }
 
+void ds1307_init(void) {
+	int seconds = 0;
+
+	i2c_start();
+	i2c_write(0xD0);      // WR to RTC
+	i2c_write(0x00);      // REG 0
+	i2c_start();
+	i2c_write(0xD1);      // RD from RTC
+	seconds = i2c_read(0); // Read current "seconds" in DS1307
+	i2c_stop();
+	seconds &= 0x7F;
+
+	delay_us(3);
+
+	i2c_start();
+	i2c_write(0xD0);      // WR to RTC
+	i2c_write(0x00);      // REG 0
+	i2c_write(seconds); // Start oscillator with current "seconds value
+	i2c_start();
+	i2c_write(0xD0);      // WR to RTC
+	i2c_write(0x07);      // Control Register
+	i2c_write(0x10);     // Disable squarewave output pin
+	i2c_stop();
+
+}
+
 int main(void) {
 
 	if (check(disp_addr)) {
@@ -59,13 +85,13 @@ int main(void) {
 		printf("\fDisp. OK...");
 	}
 
-	apagar(disp_addr);
+	ds1307_init();
 
 	while (TRUE) {
 		delay_ms(500);
 		segundos = ler_dado(sec_addr);
-		LSB = segundos & 0x0F;
-		MSB = (segundos & 0x70) >> 4;
+		LSB = (segundos | 0xF0) & 0x0F;
+		MSB = ((segundos | 0x0F) & 0x70) >> 4;
 		printf("\f%u LSB:%u MSB:%u", segundos, LSB, MSB);
 		printf("\n%u%u", MSB, LSB);
 	}
