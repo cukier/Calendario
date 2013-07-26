f30b8ab5711d0388b7b2a3b4b6e6292d8e25f6d4
cuki <mauricio@krebs.com.br>
2013-07-26 16:49:34 -0300

    adaptado para 452, ainda falta parte de escrever no calendario

--- calendario.c
+++ calendario.c
@@ -8,11 +8,12 @@
 #include<18F452.h>
 
 #fuses H4
-#use delay (crystal=8MHz, clock=32MHz)
+#use delay (crystal=10MHz, clock=40MHz)
 #use rs232(baud=9600,xmit=PIN_C6,rcv=PIN_C7)
 #use i2c(master, sda=PIN_C4, scl=PIN_C3, fast=50000)
 
 #include"calendario.h"
+#include"lcd_4b.c"
 
 struct cal calendario;
 
@@ -21,13 +22,15 @@ int main(void) {
 	int aux;
 
 	initDS1307();
+	lcd_init();
+	delay_ms(100);
 //	getDS1307(&calendario);
 
 	while (TRUE) {
 		delay_ms(500);
 		getDS1307(&calendario);
-		printf("\f%02d:%02d:%02d\n%02d/%02d/%02d", calendario.horas,
-				calendario.minutos, calendario.segundos, calendario.dia,
-				calendario.mes, calendario.ano);
+		printf(lcd, "\f        %02d:%02d:%02d\n        %02d/%02d/%02d",
+				calendario.horas, calendario.minutos, calendario.segundos,
+				calendario.dia, calendario.mes, calendario.ano);
 	}
 }
